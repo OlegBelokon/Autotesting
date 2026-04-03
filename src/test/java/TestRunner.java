3214022c320e0aba86c324ee12b74254ab755551
WhiteXopc <whitexopc@yandex.ru>
2026-04-03 22:30:49 +0300

    add test tegs

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,7 +1,12 @@
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.platform.engine.Constants;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@00001")
 public class TestRunner {
 }
