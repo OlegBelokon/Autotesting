0342e95412f980cf7437f40c0d87b98e6773db42
WhiteXopc <whitexopc@yandex.ru>
2026-04-04 23:01:17 +0300

    changeTests

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,6 +7,6 @@ import io.cucumber.junit.platform.engine.Constants;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@00002")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@00001")
 public class TestRunner {
 }
